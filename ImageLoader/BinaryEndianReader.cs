3120dd730bd758f7aaa6183a8ca3f6ce2112c87d
DKorablin <4739804+DKorablin@users.noreply.github.com>
2025-10-28 16:28:36 +0300

    Fix spelling of 'endianness' in BinaryEndianReader

--- ImageLoader/BinaryEndianReader.cs
+++ ImageLoader/BinaryEndianReader.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace AlphaOmega.Debug
@@ -6,24 +6,27 @@ namespace AlphaOmega.Debug
 	/// <summary>Binary reader with endianness check</summary>
 	public class BinaryEndianReader : BinaryReader
 	{
-		private readonly EndianHelper.Endian _endiannes;
+		private readonly EndianHelper.Endian _endianness;
 
 		/// <summary>Create instance of the binary reader specifying required endianness and input stream</summary>
 		/// <param name="endianness">Required endianness</param>
 		/// <param name="input">Byte input stream</param>
 		public BinaryEndianReader(Stream input, EndianHelper.Endian endianness)
 			: base(input)
-			=> this._endiannes = endianness;
+		{
+			this._endianness = endianness;
+		}
 
 		/// <summary>Creates reader for specific byte endianness</summary>
-		/// <param name="endiannes">Big or Little endian</param>
+		/// <param name="endianness">Big or Little endian</param>
 		/// <param name="input">Byte array stream</param>
 		/// <returns>BinaryReader</returns>
-		public static BinaryReader CreateReader(EndianHelper.Endian endiannes, MemoryStream input)
+		public static BinaryReader CreateReader(EndianHelper.Endian endianness, MemoryStream input)
 		{
-			return BitConverter.IsLittleEndian == (endiannes == EndianHelper.Endian.Little)
-				? new BinaryReader(input)
-				: new BinaryEndianReader(input, endiannes);
+			if(BitConverter.IsLittleEndian == (endianness == EndianHelper.Endian.Little))
+				return new BinaryReader(input);
+			else
+				return new BinaryEndianReader(input, endianness);
 		}
 
 		/// <summary>Reads decimal and swap bytes if needed</summary>
@@ -31,7 +34,7 @@ namespace AlphaOmega.Debug
 		public override Decimal ReadDecimal()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(Decimal));
-			EndianHelper.AdjustEndianness(typeof(Decimal), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(Decimal), bytes, this._endianness);
 			var i1 = BitConverter.ToInt32(bytes, 0);
 			var i2 = BitConverter.ToInt32(bytes, 4);
 			var i3 = BitConverter.ToInt32(bytes, 8);
@@ -45,7 +48,7 @@ namespace AlphaOmega.Debug
 		public override Double ReadDouble()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(Double));
-			EndianHelper.AdjustEndianness(typeof(Double), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(Double), bytes, this._endianness);
 
 			return BitConverter.ToDouble(bytes, 0);
 		}
@@ -55,7 +58,7 @@ namespace AlphaOmega.Debug
 		public override Int16 ReadInt16()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(Int16));
-			EndianHelper.AdjustEndianness(typeof(Int16), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(Int16), bytes, this._endianness);
 
 			return BitConverter.ToInt16(bytes, 0);
 		}
@@ -65,7 +68,7 @@ namespace AlphaOmega.Debug
 		public override Int32 ReadInt32()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(Int32));
-			EndianHelper.AdjustEndianness(typeof(Int32), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(Int32), bytes, this._endianness);
 
 			return BitConverter.ToInt32(bytes, 0);
 		}
@@ -75,7 +78,7 @@ namespace AlphaOmega.Debug
 		public override Int64 ReadInt64()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(Int64));
-			EndianHelper.AdjustEndianness(typeof(Int64), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(Int64), bytes, this._endianness);
 
 			return BitConverter.ToInt64(bytes, 0);
 		}
@@ -85,7 +88,7 @@ namespace AlphaOmega.Debug
 		public override Single ReadSingle()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(Single));
-			EndianHelper.AdjustEndianness(typeof(Single), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(Single), bytes, this._endianness);
 
 			return BitConverter.ToSingle(bytes, 0);
 		}
@@ -95,7 +98,7 @@ namespace AlphaOmega.Debug
 		public override UInt16 ReadUInt16()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(UInt16));
-			EndianHelper.AdjustEndianness(typeof(UInt16), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(UInt16), bytes, this._endianness);
 
 			return BitConverter.ToUInt16(bytes, 0);
 		}
@@ -105,7 +108,7 @@ namespace AlphaOmega.Debug
 		public override UInt32 ReadUInt32()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(UInt32));
-			EndianHelper.AdjustEndianness(typeof(UInt32), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(UInt32), bytes, this._endianness);
 
 			return BitConverter.ToUInt32(bytes, 0);
 		}
@@ -115,7 +118,7 @@ namespace AlphaOmega.Debug
 		public override UInt64 ReadUInt64()
 		{
 			Byte[] bytes = base.ReadBytes(sizeof(UInt64));
-			EndianHelper.AdjustEndianness(typeof(UInt64), bytes, this._endiannes);
+			EndianHelper.AdjustEndianness(typeof(UInt64), bytes, this._endianness);
 
 			return BitConverter.ToUInt64(bytes, 0);
 		}
